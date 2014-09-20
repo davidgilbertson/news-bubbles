07a4d4db37c12bda88ef65dffca1f707674d977e
David Gilbertson <gilbertson.david@gmail.com>
2014-09-20 15:55:48 +1000

    turned off hacker news crawler for testing

--- server/server.js
+++ server/server.js
@@ -26,7 +26,7 @@ exports.start = function(app) {
 
   db.on('open', function() {
     console.log('Database connection opened.');
-    hxnCrawler.startCrawler(io);
+    // hxnCrawler.startCrawler(io);
     rdtCrawler.startCrawler(io);
     http.listen(port);
 
