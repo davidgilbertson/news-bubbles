de32459b3ee1e07fce88454d240b0771fabd924b
David Gilbertson <gilbertson.david@gmail.com>
2014-10-05 09:44:55 +1100

    Added localStorage versioning

--- app/scripts/init.js
+++ app/scripts/init.js
@@ -1,6 +1,8 @@
 'use strict';
 var NB = NB || {};
 
+//Everything here runs before anything else is initialized
+
 //Constants
 NB.DUR_FAST = 200; //should match _variables.scss duration variable
 NB.DUR_SLOW = 2000;
@@ -15,3 +17,12 @@ if (!!document.location.host.match(/localhost/)) {
 
 NB.hasTouch = false;
 NB.oldestStory = Infinity;
+
+
+var targetLocalStorageVersion = 1; //increment this to wipe the localstorage for older versions
+var lsVersion = localStorage.v ? localStorage.v : 0;
+if (lsVersion < targetLocalStorageVersion) {
+  console.log('Clearing local storage.');
+  localStorage.clear();
+  localStorage.v = targetLocalStorageVersion;
+}
