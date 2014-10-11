3d262b12fb67bdbde63605938e53ea13212d7c1e
David Gilbertson <gilbertson.david@gmail.com>
2014-10-11 16:19:41 +1100

    Inline youtube in comments!

--- app/scripts/comments.js
+++ app/scripts/comments.js
@@ -23,6 +23,9 @@ NB.Comments = (function() {
     //turn any imgur link without jpg into jpg
     result = result.replace(/(<a [^>]*?href=")(http:\/\/imgur\.com\/[^./]*?)(".*?)(<\/a>)/g, '$1$2$3<img src="$2.jpg">$4');
 
+    //Replace YouTube links with YouTube videos
+    result = result.replace(/<a[^>]*?www\.youtube\.com\/watch\?v=([^?&"]*).*<\/a>/g, '<iframe width="560" height="315" src="//www.youtube.com/embed/$1" frameborder="0" allowfullscreen></iframe>');
+
     return result;
   }
 
